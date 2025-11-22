88e8297fe52b59c50696acd53b0de14c2d6e206a
yuzu-krs <yuzuto.poi@gmail.com>
2025-11-23 06:21:57 +0900

    home-component-add

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {Home} from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: '<h1>Hello World!</h1>',
-  styleUrl: './app.css'
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.css'],
 })
 export class App {
   title = 'homes';
